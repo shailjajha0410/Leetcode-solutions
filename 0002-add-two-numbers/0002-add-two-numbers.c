ad71971d0521293e3d3d89f572f982105f5b75d7
shailjajha0410 <shailjajha0410@gmail.com>
2026-05-18 00:38:51 +0530

    Time: 0 ms (100%), Space: 13.5 MB (20.3%) - LeetHub - Time: O(max(m, n)), Space: O(max(m, n))

---
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -0,0 +1,41 @@
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+
+    struct ListNode *dummy =
+        (struct ListNode*)malloc(sizeof(struct ListNode));
+
+    struct ListNode *temp = dummy;
+
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        struct ListNode *newNode =
+            (struct ListNode*)malloc(sizeof(struct ListNode));
+
+        newNode->val = sum % 10;
+        newNode->next = NULL;
+
+        temp->next = newNode;
+        temp = temp->next;
+    }
+
+    return dummy->next;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
