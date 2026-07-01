8d5ed44a4ab7ccdb4940449ba788270d68b0859e
Shailja Jha <shailjajha0410@gmail.com>
2026-07-01 14:48:40 +0530

    Update 0045_Jump_GameII.py

--- LeetCode Python Solutions/0045_Jump_GameII.py
+++ LeetCode Python Solutions/0045_Jump_GameII.py
@@ -1,1 +1,18 @@
+class Solution:
+    def jump(self, nums):
+        jumps = 0
+        left = 0
+        right = 0
 
+        while right < len(nums) - 1:
+            farthest = 0
+
+            for i in range(left, right + 1):
+                farthest = max(farthest, i + nums[i])
+
+            left = right + 1
+            right = farthest
+
+            jumps += 1
+
+        return jumps
