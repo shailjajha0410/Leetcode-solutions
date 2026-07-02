6d7bbeff3ed0f8ba6910b169d47eafd55d5a9bdf
Shailja Jha <shailjajha0410@gmail.com>
2026-07-02 15:44:41 +0530

    Update 0067_Add_Binary.py

--- LeetCode Python Solutions/0067_Add_Binary.py
+++ LeetCode Python Solutions/0067_Add_Binary.py
@@ -1,22 +1,3 @@
-class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        i = len(a) - 1
-        j = len(b) - 1
-        carry = 0
-        res = []
-
-        while i >= 0 or j >= 0 or carry:
-            total = carry
-
-            if i >= 0:
-                total += a[i] == '1'
-                i -= 1
-
-            if j >= 0:
-                total += b[j] == '1'
-                j -= 1
-
-            res.append('1' if total & 1 else '0')
-            carry = total >> 1
-
-        return ''.join(reversed(res))
+class Solution(object):
+    def addBinary(self, a, b):
+        return bin(int(a,2)+int(b,2))[2:]
