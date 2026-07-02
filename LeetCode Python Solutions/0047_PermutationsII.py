b0eb93a4874985a4615fc1dcaff9568fc151bcac
Shailja Jha <shailjajha0410@gmail.com>
2026-07-02 15:54:05 +0530

    Update 0047_PermutationsII.py

--- LeetCode Python Solutions/0047_PermutationsII.py
+++ LeetCode Python Solutions/0047_PermutationsII.py
@@ -1,1 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums):
+        result = []
 
+        def backtrack(path, nums):
+            if not nums:
+                result.append(path)
+                return
+
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+
+                backtrack(
+                    path + [nums[i]],
+                    nums[:i] + nums[i + 1:]
+                )
+
+        backtrack([], sorted(nums))
+        return result
