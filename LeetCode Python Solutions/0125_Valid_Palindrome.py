90ed163676b889c041a64e838a6c4a1615a88331
Shailja Jha <shailjajha0410@gmail.com>
2026-07-04 16:06:22 +0530

    Update 0125_Valid_Palindrome.py

--- LeetCode Python Solutions/0125_Valid_Palindrome.py
+++ LeetCode Python Solutions/0125_Valid_Palindrome.py
@@ -1,1 +1,7 @@
-
+import re
+class Solution(object):
+    def isPalindrome(self, s):
+        s = s.lower().strip()
+        s = s.replace(" ", "")
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        return s == s[::-1]    
