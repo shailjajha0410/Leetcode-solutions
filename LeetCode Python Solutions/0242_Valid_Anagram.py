e8c17b3b22f85e273a0480016f15bbf01a7ba7a8
Shailja Jha <shailjajha0410@gmail.com>
2026-07-04 14:54:53 +0530

    Update 0242_Valid_Anagram.py

--- LeetCode Python Solutions/0242_Valid_Anagram.py
+++ LeetCode Python Solutions/0242_Valid_Anagram.py
@@ -1,1 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
 
+        # Set (unique letters)
+        setS = set(s)
+
+        # for each unique letter
+        for char in setS:
+            # if unique char count is not equal
+            if s.count(char) != t.count(char):
+                return False
+        return True
