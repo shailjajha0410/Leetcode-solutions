0dc2b38b72f2a1823989d4d42d1dc70c6f91c01b
Shailja Jha <shailjajha0410@gmail.com>
2026-07-05 16:13:24 +0530

    Update 0853_Car_Fleet.py

--- LeetCode Python Solutions/0853_Car_Fleet.py
+++ LeetCode Python Solutions/0853_Car_Fleet.py
@@ -1,1 +1,13 @@
+class Solution:
+    def carFleet(self, target, position, speed):
+        cars = sorted(zip(position, speed), reverse=True)
+        stack = []
 
+        for pos, spd in cars:
+            time = (target - pos) / float(spd)
+            stack.append(time)
+
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+
+        return len(stack)
