34d49fab8e91e3fb653d9044deeb5ffbab6ea32c
Shailja Jha <shailjajha0410@gmail.com>
2026-07-05 14:57:33 +0530

    Add twoSum method to Solution class
    Implement twoSum method to find indices of two numbers that add up to target.

--- LeetCode Python Solutions/0167_Two_SumII.py
+++ LeetCode Python Solutions/0167_Two_SumII.py
@@ -1,1 +1,16 @@
 # two-sum-ii-input-array-is-sorted
+class Solution:
+    def twoSum(self, numbers, target):
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            total = numbers[left] + numbers[right]
+
+            if total == target:
+                return [left + 1, right + 1]
+
+            elif total < target:
+                left += 1
+
+            else:
+                right -= 1
