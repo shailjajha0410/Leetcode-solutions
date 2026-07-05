b5db0bf347c36581bc824a97c60ed9bde2fb5e74
Shailja Jha <shailjajha0410@gmail.com>
2026-07-05 16:00:00 +0530

    Update 0150_Evaluate_Reverse_Polish_Notations.py

--- LeetCode Python Solutions/0150_Evaluate_Reverse_Polish_Notations.py
+++ LeetCode Python Solutions/0150_Evaluate_Reverse_Polish_Notations.py
@@ -1,1 +1,18 @@
-
+from math import ceil,floor
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack=[]
+        for t in tokens:
+            if t in "+-*/":
+                b,a=stack.pop(),stack.pop()
+                if t=='+':
+                    stack.append(a+b)
+                elif t=='-':
+                    stack.append(a-b)
+                elif t=='*':
+                    stack.append(a*b)
+                else:
+                    stack.append(int(float(a) / b)) 
+            else:
+                stack.append(int(t))
+        return stack[0]   
