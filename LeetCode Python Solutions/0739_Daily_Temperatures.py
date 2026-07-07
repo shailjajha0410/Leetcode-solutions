53ebb0693500d0f5e4c38a198e1f9c4859c04162
Shailja Jha <shailjajha0410@gmail.com>
2026-07-07 16:51:13 +0530

    Update 0739_Daily_Temperatures.py

--- LeetCode Python Solutions/0739_Daily_Temperatures.py
+++ LeetCode Python Solutions/0739_Daily_Temperatures.py
@@ -1,1 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        res = [0] * len(temperatures)
+        stack = []
 
+        for i, temp in enumerate(temperatures):
+
+            while stack and temp > stack[-1][0]:
+                stackTemp, stackIndex = stack.pop()
+                res[stackIndex] = i - stackIndex
+
+            stack.append((temp, i))
+
+        return res
