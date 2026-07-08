fc277aca4a30d93e8e700013df5187b64fce0c3a
Shailja Jha <shailjajha0410@gmail.com>
2026-07-08 15:08:34 +0530

    Add solution for maximum average subarray II
    Implement a solution to find the maximum average subarray of length k.

--- LeetCode Python Solutions/0643_Maximum_Average_SubarrayII.py
+++ LeetCode Python Solutions/0643_Maximum_Average_SubarrayII.py
@@ -1,1 +1,10 @@
+class Solution:
+    def findMaxAverage(self, nums, k):
+        windowSum = sum(nums[:k])          
+        maxSum = windowSum
 
+        for i in range(k, len(nums)):
+            windowSum += nums[i] - nums[i - k]           
+            maxSum = max(maxSum, windowSum)
+
+        return maxSum / k
