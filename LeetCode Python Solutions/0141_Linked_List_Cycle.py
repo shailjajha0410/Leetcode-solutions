a639f1a8157436842c9e2012d7050ab952d5bf9e
Shailja Jha <shailjajha0410@gmail.com>
2026-07-08 15:19:34 +0530

    Update 0141_Linked_List_Cycle.py

--- LeetCode Python Solutions/0141_Linked_List_Cycle.py
+++ LeetCode Python Solutions/0141_Linked_List_Cycle.py
@@ -1,1 +1,11 @@
+class Solution(object):
+    def hasCycle(self, head):
+        slow = head   #slow moves 1 step at a time (starts from head)
+        fast = head   #fast moves 2 steps at a time (starts from head)
 
+        while fast and fast.next:
+            slow = slow.next  # move next step
+            fast = fast.next.next   #move two steps
+            if slow == fast:  #if both reaches at one point ever
+                return True   #there is a loop
+        return False     #there ain't a loop
