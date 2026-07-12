b729384ccac85d0e9e30efd9f87cdefb08b8993f
Shailja Jha <shailjajha0410@gmail.com>
2026-07-12 14:57:49 +0530

    Update 0543_Diameter_of_Binary_Tree.py

--- LeetCode Python Solutions/0543_Diameter_of_Binary_Tree.py
+++ LeetCode Python Solutions/0543_Diameter_of_Binary_Tree.py
@@ -1,1 +1,17 @@
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        self.diameter = 0
 
+        def height(node):
+            if not node:
+                return 0
+
+            left = height(node.left)
+            right = height(node.right)
+
+            self.diameter = max(self.diameter, left + right)
+
+            return 1 + max(left, right)
+
+        height(root)
+        return self.diameter
