8b7b679b754a7103e301b6faa2cbd7bf01cc8b45
Shailja Jha <shailjajha0410@gmail.com>
2026-07-12 15:12:27 +0530

    Update 0257_Binary_Tree_Paths.py

--- LeetCode Python Solutions/0257_Binary_Tree_Paths.py
+++ LeetCode Python Solutions/0257_Binary_Tree_Paths.py
@@ -1,1 +1,21 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        res = []
 
+        def dfs(node, path):
+            if not node:
+                return
+
+            path += str(node.val)
+
+            if not node.left and not node.right:
+                res.append(path)
+                return
+
+            path += "->"
+
+            dfs(node.left, path)
+            dfs(node.right, path)
+
+        dfs(root, "")
+        return res
