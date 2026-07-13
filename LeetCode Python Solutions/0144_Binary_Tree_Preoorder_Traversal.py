9eb9888704bdc495aa2bdda0f5165a9bc35b210f
Shailja Jha <shailjajha0410@gmail.com>
2026-07-13 17:10:01 +0530

    Update 0144_Binary_Tree_Preoorder_Traversal.py

--- LeetCode Python Solutions/0144_Binary_Tree_Preoorder_Traversal.py
+++ LeetCode Python Solutions/0144_Binary_Tree_Preoorder_Traversal.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def preorderTraversal(self, root):
+        result = []
+        
+        def dfs(node):
+            if not node:
+                return
+            
+            # 1. Visit the root
+            result.append(node.val)
+            # 2. Traverse left
+            dfs(node.left)
+            # 3. Traverse right
+            dfs(node.right)
+            
+        dfs(root)
+        return result
